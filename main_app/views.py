dd29b28d1efc248e3f6a6fc42f051efd4f631962
clairecleverlamb <clairechenszeying@gmail.com>
2025-05-01 13:05:13 -0700

    full crud function

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 # Create your views here. view function 
@@ -26,3 +27,19 @@ def cat_detail(request, cat_id):
     return render(request, 'cats/detail.html', {
         "cat": cat 
     })
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+
+
+
